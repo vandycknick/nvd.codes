86fa07dd92441bb061237d8412aec7f557238a4a
Nick Van Dyck <vandyck.nick@outlook.com>
2020-03-31 21:47:38 +0200

    chore: update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,8 +98,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `nvd.codes`,
+        short_name: `Nick Van Dyck's personal website and blog`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
