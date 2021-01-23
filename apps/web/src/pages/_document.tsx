b871218002e971484a8a4992ad31cd9826384ba8
Nick Van Dyck <vandyck.nick@outlook.com>
2021-01-23 18:09:55 +0100

    chore: update dependencies

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -7,33 +7,25 @@ import Document, {
   NextScript,
   DocumentContext,
 } from "next/document"
-import { extractCritical } from "@emotion/server"
 
 import { GA_TRACKING_ID } from "services/gtag"
 import theme from "theme"
 
-type CriticalStyles = ReturnType<typeof extractCritical>
-type DocumentProps = { html: string } & CriticalStyles
+type DocumentProps = { html: string }
 
 class MyDocument extends Document<DocumentProps> {
   static async getInitialProps({
     renderPage,
   }: DocumentContext): Promise<DocumentProps> {
     const page = await renderPage()
-    const styles = extractCritical(page.html)
-    return { ...page, ...styles }
+    return { ...page }
   }
 
   render(): JSX.Element {
-    const { ids, css } = this.props
     return (
       <Html lang="en">
         <Head>
           <link rel="shortcut icon" href="/favicon.png" />
-          <style
-            data-emotion-css={ids.join(" ")}
-            dangerouslySetInnerHTML={{ __html: css }}
-          />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
