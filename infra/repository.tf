226a79ad14fcca7e29c9ea29e4e8e8f3c4cf39b3
Nick Van Dyck <vandyck.nick@outlook.com>
2021-07-02 18:24:25 +0200

    fix: immutable not supported

--- infra/repository.tf
+++ infra/repository.tf
@@ -2,6 +2,6 @@ resource "oci_artifacts_container_repository" "test_container_repository" {
   compartment_id = oci_identity_compartment.nvd_codes.id
   display_name   = "nvd-codes-repo"
 
-  is_immutable = true
+  is_immutable = false
   is_public    = false
 }
