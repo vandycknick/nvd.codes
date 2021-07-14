51c9c4e7b5b1ecb979516fc9b086a43e062715e5
Nick Van Dyck <vandyck.nick@outlook.com>
2021-07-15 00:18:34 +0200

    feat(infra): add container repo for api and blog

---
+++ infra/repositories.tf
@@ -0,0 +1,15 @@
+resource "oci_artifacts_container_repository" "nvd_codes_api" {
+  compartment_id = oci_identity_compartment.nvd_codes.id
+  display_name   = "nvd-codes/api"
+
+  is_immutable = false
+  is_public    = false
+}
+
+resource "oci_artifacts_container_repository" "nvd_codes_blog" {
+  compartment_id = oci_identity_compartment.nvd_codes.id
+  display_name   = "nvd-codes/blog"
+
+  is_immutable = false
+  is_public    = false
+}
