871d6948a7e3d8366445c2373ad789dc3c109532
Nick Van Dyck <vandyck.nick@outlook.com>
2021-07-14 23:47:31 +0200

    chore(infra): tmp remove db

--- infra/database.tf
+++ infra/database.tf
@@ -1,21 +1,21 @@
-resource "oci_database_autonomous_database" "nvd_codes_blog_db" {
-  compartment_id           = oci_identity_compartment.nvd_codes.id
-  db_name                  = "nvdcodesdb"
-  display_name             = "nvdcodesdb"
-  db_version               = "19c"
-  db_workload              = "DW"
-  cpu_core_count           = 1
-  data_storage_size_in_gb  = 20
-  data_storage_size_in_tbs = 1
+# resource "oci_database_autonomous_database" "nvd_codes_blog_db" {
+#   compartment_id           = oci_identity_compartment.nvd_codes.id
+#   db_name                  = "nvdcodesdb20210714213708"
+#   display_name             = "nvdcodesdb"
+#   db_version               = "19c"
+#   db_workload              = "DW"
+#   cpu_core_count           = 1
+#   data_storage_size_in_gb  = 20
+#   data_storage_size_in_tbs = 1
 
-  admin_password = var.database_admin_password
+#   admin_password = var.database_admin_password
 
-  is_auto_scaling_enabled = false
-  is_data_guard_enabled   = false
-  is_dedicated            = false
-  is_free_tier            = true
+#   is_auto_scaling_enabled = false
+#   is_data_guard_enabled   = false
+#   is_dedicated            = false
+#   is_free_tier            = true
 
-  license_model = "LICENSE_INCLUDED"
+#   license_model = "LICENSE_INCLUDED"
 
-  whitelisted_ips = concat(["${module.vcn.vcn_id};${module.subnet_addrs.network_cidr_blocks.node_subnet}"], var.database_ip_safe_list)
-}
+#   whitelisted_ips = concat(["${module.vcn.vcn_id};${module.subnet_addrs.network_cidr_blocks.node_subnet}"], var.database_ip_safe_list)
+# }
