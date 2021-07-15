245169d44252ef6750470eb1f63ee1920af82ff8
Nick Van Dyck <vandyck.nick@outlook.com>
2021-07-15 23:26:24 +0200

    feat(blog): load oci api creds from env

--- apps/blog/src/config.ts
+++ apps/blog/src/config.ts
@@ -4,10 +4,15 @@ export const getConfig = memoize(() => {
   return {
     bucketNamespace: getEnvVar("IMAGES_BUCKET_NAMESPACE"),
     bucketName: getEnvVar("IMAGES_BUCKET_NAME"),
-    port: getEnvVarAsInt("PORT", 4000),
+    port: getEnvVarAsInt("PORT", 3000),
     dbType: getEnvVar("TYPEORM_CONNECTION") as "oracle",
     dbUser: getEnvVar("TYPEORM_USERNAME"),
     dbPassword: getEnvVar("TYPEORM_PASSWORD"),
     dbDriverConfig: getEnvVar("TYPEORM_DRIVER_EXTRA"),
+    ociUser: getEnvVar("OCI_USER"),
+    ociTenancy: getEnvVar("OCI_TENANCY"),
+    ociFingerprint: getEnvVar("OCI_FINGERPRINT"),
+    ociRegion: getEnvVar("OCI_REGION"),
+    ociPrivateKey: getEnvVar("OCI_PRIVATE_KEY"),
   }
 })
