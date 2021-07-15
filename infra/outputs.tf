f5a40d1905f36efddd868cbb297e562ea20bdcf8
Nick Van Dyck <vandyck.nick@outlook.com>
2021-07-15 09:03:18 +0200

    chore(infra): mark wallet output as sensitive

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,4 @@
 output "database_wallet_zip" {
-  value = oci_database_autonomous_database_wallet.nvd_codes_blog_db_wallet.content
+  value     = oci_database_autonomous_database_wallet.nvd_codes_blog_db_wallet.content
+  sensitive = true
 }
