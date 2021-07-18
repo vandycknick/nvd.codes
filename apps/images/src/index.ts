3ade576b205245dc25deedd541626011f35b421a
Nick Van Dyck <vandyck.nick@outlook.com>
2021-07-18 09:48:26 +0200

    refactor(images): use oci user from env

--- apps/images/src/index.ts
+++ apps/images/src/index.ts
@@ -1,11 +1,14 @@
 import Koa from "koa"
+import Router from "@koa/router"
 import loggerMiddleware from "koa-bunyan-logger"
 import { createLogger } from "bunyan"
 
 import { getConfig } from "./config"
 import imageOptimizer from "./controllers/imageOptimizer"
+import { getHealth } from "./controllers/getHealth"
 
 const app = new Koa()
+const router = new Router()
 const config = getConfig()
 const logger = createLogger({
   name: "app",
@@ -24,12 +27,22 @@ app.use(async (ctx, next) => {
   })
 })
 
-app.use(async (context) => {
-  const result = await imageOptimizer(context)
+router.get("/health", async (ctx) => {
+  const result = await getHealth(ctx)
+  ctx.set(result.headers ?? {})
+  ctx.status = result.status
+  ctx.body = result.body
+})
 
-  context.set(result.headers ?? {})
-  context.status = result.status
-  context.body = result.body
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.use(async (ctx) => {
+  const result = await imageOptimizer(ctx)
+
+  ctx.set(result.headers ?? {})
+  ctx.status = result.status
+  ctx.body = result.body
 })
 
 app.listen(config.port, () =>
