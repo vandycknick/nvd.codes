bde7ca3072b54d5ccdc8773a7a3e3429333ffdce
Nick Van Dyck <vandyck.nick@outlook.com>
2021-07-18 09:59:11 +0200

    feat(infra): add repo for images and web app

--- infra/repositories.tf
+++ infra/repositories.tf
@@ -13,3 +13,19 @@ resource "oci_artifacts_container_repository" "nvd_codes_blog" {
   is_immutable = false
   is_public    = false
 }
+
+resource "oci_artifacts_container_repository" "nvd_codes_images" {
+  compartment_id = oci_identity_compartment.nvd_codes.id
+  display_name   = "nvd-codes/images"
+
+  is_immutable = false
+  is_public    = false
+}
+
+resource "oci_artifacts_container_repository" "nvd_codes_web" {
+  compartment_id = oci_identity_compartment.nvd_codes.id
+  display_name   = "nvd-codes/web"
+
+  is_immutable = false
+  is_public    = false
+}
