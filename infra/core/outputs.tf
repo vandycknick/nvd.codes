6dc404e21be5b931c4eefb9ec7d23741d59d0af4
Nick Van Dyck <vandyck.nick@outlook.com>
2021-07-21 07:50:38 +0200

    chore(infra): add compartment id as output to core

--- infra/core/outputs.tf
+++ infra/core/outputs.tf
@@ -1,3 +1,7 @@
+output "compartment_id" {
+  value = oci_identity_compartment.nvd_codes.id
+}
+
 output "database_wallet_zip" {
   value     = oci_database_autonomous_database_wallet.nvd_codes_blog_db_wallet.content
   sensitive = true
