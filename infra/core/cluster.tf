a27539307691dd34a2008bcd0d4e7e8c3cd2f363
Nick Van Dyck <vandyck.nick@outlook.com>
2021-09-19 09:27:04 +0200

    chore(infra): update image to latest version

--- infra/core/cluster.tf
+++ infra/core/cluster.tf
@@ -59,17 +59,17 @@ resource "oci_containerengine_node_pool" "nvd_codes_pool_1" {
 }
 
 resource "oci_containerengine_node_pool" "nvd_codes_pool_2" {
-  count              = 0
+  count              = 1
   cluster_id         = oci_containerengine_cluster.nvd_codes_cluster.id
   compartment_id     = oci_identity_compartment.nvd_codes.id
-  kubernetes_version = "v1.19.12"
+  kubernetes_version = "v1.20.8"
   name               = "nvd-codes-pool-2"
   node_config_details {
     placement_configs {
       availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
       subnet_id           = oci_core_subnet.node_subnet.id
     }
-    size = 3
+    size = 1
   }
   node_shape = "VM.Standard.A1.Flex"
 
@@ -79,8 +79,8 @@ resource "oci_containerengine_node_pool" "nvd_codes_pool_2" {
   }
 
   node_source_details {
-    # https://docs.oracle.com/en-us/iaas/images/image/f3727add-b1a2-47d8-8064-a1a9741096a0/
-    image_id    = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaay4mtwt7fafcm7bdmxweoldowmwyudbbnfjqtvnfvy4wiewsfvueq"
+    # https://docs.oracle.com/en-us/iaas/images/image/3ca33eae-c31d-4dac-b86e-914e32b19fb2/
+    image_id    = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaaavj6losrzcyol3spv3da2xf6qskfizyqw7fajq5xekimbcn74gsa"
     source_type = "image"
   }
 
