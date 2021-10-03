27318b73c05ec742e0b019f0e779cf0bee946079
Nick Van Dyck <vandyck.nick@outlook.com>
2021-10-03 23:14:26 +0200

    feat(web): seo improvements

--- apps/web/src/components/Common/SEO.tsx
+++ apps/web/src/components/Common/SEO.tsx
@@ -1,19 +1,24 @@
 import React from "react"
-import { NextSeo } from "next-seo"
+import { NextSeo, NextSeoProps } from "next-seo"
 
 interface SEOProps {
   title: string
   description?: string
+  openGraph?: NextSeoProps["openGraph"]
 }
 
-const SEO: React.FC<SEOProps> = ({ title, description }) => (
+const SEO: React.FC<SEOProps> = ({ title, description, openGraph }) => (
   <NextSeo
     title={title}
     description={description}
     twitter={{
-      handle: "@handle",
-      site: "@site",
+      handle: "@vandycknick",
+      site: "@vandycknick",
       cardType: "summary_large_image",
+    }}
+    openGraph={{
+      site_name: "Nick Van Dyck | nvd.codes",
+      ...openGraph,
     }}
   />
 )
