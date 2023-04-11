02502afe033eb0550b359c4f730fd341f73c307b
Nick Van Dyck <vandyck.nick@outlook.com>
2023-04-12 00:39:46 +0200

    feat(web): remove placeholder content  on uses page

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -24,7 +24,7 @@ export function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            I&aposm Nick Van Dyck. A Software Engineer working out of Belgium.
+            I&apos;m Nick Van Dyck. A Software Engineer working out of Belgium.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
