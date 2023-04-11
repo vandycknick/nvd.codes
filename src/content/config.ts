68d9590467858a191afed967937ca669a1c38885
Nick Van Dyck <vandyck.nick@outlook.com>
2023-04-11 21:44:47 +0200

    chore(web): update astro and dependencies

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,15 @@
-import { z, defineCollection, image } from "astro:content"
+import { z, defineCollection } from "astro:content"
 
 const blog = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.date(),
-    categories: z.array(z.string()),
-    cover: image(),
-    draft: z.boolean().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z.date(),
+      categories: z.array(z.string()),
+      cover: image(),
+      draft: z.boolean().optional(),
+    }),
 })
 
 export const collections = {
